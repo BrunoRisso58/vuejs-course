d2c8d5c446c037e4f081427e7385a531be062976
Bruno Risso <brunorisso58@gmail.com>
2023-10-31 12:02:55 -0300

    Project 3 - updated

--- 3-TodoList/scripts.js
+++ 3-TodoList/scripts.js
@@ -1,14 +1,7 @@
-var tasks = [
-    {
-        name: "Aprender a cozinhar",
-        done: false
-    },
-]
-
 const handleTasks = {
     data() {
         return {
-            tasks: window.tasks,
+            tasks: [],
             newTask: {}
         }
     },
@@ -20,11 +13,25 @@ const handleTasks = {
             if (this.newTask.name) {
                 this.tasks.push({ name: this.newTask.name, done: false });
                 this.newTask = {};
+                localStorage.setItem('tasks', JSON.stringify(this.tasks)); // transforma o objeto em json, pois o localstorage só guarda strings
             } else {
                 alert('Preencha o nome da tarefa');
             }
         }
+    },
+    created() {
+        this.tasks = JSON.parse(localStorage.getItem('tasks')); // transforma o json em objeto
+    },
+    updated() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks)); // atualiza o item tasks no localStorage
     }
 }
 
 Vue.createApp(handleTasks).mount('#app');
+
+// Lifecycle hooks
+// beforeCreate: chamado quando a instância é inicializada mas antes de processar data()
+// created: chamado quando data() etc é processada (ainda não gerou o html)
+// beforeUpdate: imediatamente antes de alguma alteração ser feita
+// updated: quando uma alteração é feita
+// cada componente tem o seu ciclo de vida individualmente
